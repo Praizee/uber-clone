0ec009d609fc4bd53fb8a409d598e8d4bd8052bc
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-02-11 15:30:29 +0100

    screens

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,19 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Image } from "react-native";
 import React from "react";
+import NavOptions from "../components/NavOptions";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView className="flex-1 justify-center items-center">
-      <Text className="text-4xl font-bold text-primary">Uber Clone!!</Text>
+    <SafeAreaView className="bg-white h-full ">
+      <View className="p-5">
+        <Image
+          source={{ uri: "https://links.papareact.com/gzs" }}
+          className=""
+          style={{ width: 100, height: 100, resizeMode: "contain" }}
+        />
+
+        <NavOptions />
+      </View>
     </SafeAreaView>
   );
 };
