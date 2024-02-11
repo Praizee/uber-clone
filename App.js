4f2acc56b423b873a4c8f880be70592ec42b194a
Stephen Adeniji <praiseadeniji2017@gmail.com>
2024-02-11 19:05:54 +0100

    google-places-autocomplete

--- App.js
+++ App.js
@@ -6,6 +6,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import MapScreen from "./screens/MapScreen";
 import FoodScreen from "./screens/FoodScreen";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 export default function App() {
   const Stack = createStackNavigator();
@@ -14,7 +16,7 @@ export default function App() {
   // https://developers.google.com/maps/documentation/places/web-service/get-api-key/
   // https://console.cloud.google.com/apis/library/directions-backend.googleapis.com?organizationId=0&project=uber-clone-app-414016
 
-  https: return (
+  return (
     <Provider store={store}>
       <NavigationContainer>
         <SafeAreaProvider>
